36158579b3008b704b719d2531f1bfbcf39078c0
Ravi Kant Prasad <63839002+ravi0818@users.noreply.github.com>
2024-08-13 20:43:04 +0530

    APIs - Doctor (#6)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import verifyToken from '@middlewares/authMiddleware';
 
 import authRoutes from './authRoutes';
 import clinicRoutes from './clinicRoutes';
+import doctorRoutes from './doctorRoutes';
 import patientRoutes from './patientRoutes';
 import userRoutes from './userRoutes';
 
@@ -15,5 +16,6 @@ router.use(verifyToken);
 router.use('/user', userRoutes);
 router.use('/patient', patientRoutes);
 router.use('/clinic', clinicRoutes);
+router.use('/doctor', doctorRoutes);
 
 export default router;
